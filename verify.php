9b4bafd6dee0b7ebf18839ac53ce89c3d4f3c38b
rathrawee <tsornwichai@hotmail.com>
2017-03-08 11:05:33 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,5 @@
 <?php
-$access_token = '2/2pH5VW3ug1FalmM9cw8GxR8vNZ/OuaywBlMWM2JgB/l
-
-+fhClbRI8UVVrtuUKrI8AIAoVDx7VUqr4Z5Ep21JFd3MQbeddh2Fb63oj
-
-+r2fDEnRdMKQZ/LaFkSz2FXQy4zWGCJqmvbjUOYQUrJW3EVwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'U27fc888c4fd40c38e98dd576f9cd53e3';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
