ec32b1ca7e4f58a1c2650ac7581b3f4cc46d1496
rathrawee <tsornwichai@hotmail.com>
2017-03-08 11:08:38 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'r1s0VtFGNoTaGt4PofFerg9X1ro5Wrtw1XwkSPZSh4e';
+$access_token = '2/2pH5VW3ug1FalmM9cw8GxR8vNZ/OuaywBlMWM2JgB/l+fhClbRI8UVVrtuUKrI8AIAoVDx7VUqr4Z5Ep21JFd3MQbeddh2Fb63oj+r2fDEnRdMKQZ/LaFkSz2FXQy4zWGCJqmvbjUOYQUrJW3EVwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
